509f3dac1b3e834675a989db1bc8e7030a534a3b
Angel YG <angel.yarang4@gmail.com>
2024-11-21 02:55:08 -0500

    feat: slider done

--- src/components/react/home/HeroSlider.jsx
+++ src/components/react/home/HeroSlider.jsx
@@ -1,38 +1,46 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.css'
-// import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper'
 
-// Usa los módulos de Swiper
-// SwiperCore.use([Navigation, Pagination, Autoplay])
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+
+import ArrowRightIcon from '../icons/ArrowRightIcon'
 
 const HeroSlider = () => {
   const slides = [
-    { id: 1, content: 'Slide 1' },
-    { id: 2, content: 'Slide 2' },
-    { id: 3, content: 'Slide 3' }
+    { id: 1, content: 'Slide 1', image: '/assets/images/sliders/hero_1.png' },
+    { id: 2, content: 'Slide 2', image: '/assets/images/sliders/hero_1.png' },
+    { id: 3, content: 'Slide 3', image: '/assets/images/sliders/hero_1.png' }
   ]
 
   return (
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={10}
-      navigation
-      pagination={{ clickable: true }}
-      loop={true} // Habilitar el bucle infinito
-      autoplay={{
-        // Configuración de autoplay
-        delay: 3000, // Tiempo en milisegundos
-        disableOnInteraction: false // Permitir la interacción sin detener el autoplay
-      }}>
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <div className="flex h-64 items-center justify-center bg-gray-200">
-            <h2 className="text-lg">{slide.content}</h2>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative w-full rounded-md border">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={true}
+        navigation={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false
+        }}
+        pagination={{
+          clickable: true
+        }}>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className="flex h-96 items-center justify-center overflow-hidden rounded-2xl">
+              <img
+                src={slide.image}
+                alt={`Slide ${slide.id}`}
+                className="h-full w-full transform object-cover transition-transform duration-500 ease-in-out hover:scale-105"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
 
