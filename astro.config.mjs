5e44d22bedbb8a5e3442565552af86d8fe5c59a6
Angel YG <angel.yarang4@gmail.com>
2024-11-24 07:19:24 -0500

    setting server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 })
